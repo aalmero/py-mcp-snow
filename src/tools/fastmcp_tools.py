08a2c92ba3d830d5f5eab6379e872f0e07b1482d
Alex Almero <alex.almero@lafarge.com>
2026-01-22 12:27:48 -0400

    fix CORS wrapping

--- src/tools/fastmcp_tools.py
+++ src/tools/fastmcp_tools.py
@@ -30,6 +30,8 @@ middleware = [
     )
 ]
 
+app = mcp.http_app(middleware=middleware)
+
 # Global client instance (will be initialized by server)
 servicenow_client: Optional[ServiceNowClient] = None
 
